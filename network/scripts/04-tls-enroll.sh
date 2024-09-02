24febafe0317c759d3576484a84f08391ca4cc16
Mikhail Mitskevich <mitskevichmn@gmail.com>
2024-09-02 10:59:47 +0300

    Unify ca ports

--- network/scripts/04-tls-enroll.sh
+++ network/scripts/04-tls-enroll.sh
@@ -2,5 +2,7 @@ export PATH="$(dirname $(readlink -e ./))/bin:$PATH"
 export FABRIC_CA_CLIENT_HOME=`pwd`/client
 
 IP=${2:-10.131.44.144}
+SERVER=${3:-10.131.44.144}
+PORT=${4:-7053}
 
-fabric-ca-client enroll -d -u https://$1:$1pw@localhost:7053 --tls.certfiles tls-root-cert/tls-ca-cert.pem --enrollment.profile tls --csr.hosts "apmi5,localhost,$IP" --mspdir tls-ca/$1/msp
+fabric-ca-client enroll -d -u https://$1:$1pw@$SERVER:$PORT --tls.certfiles tls-root-cert/tls-ca-cert.pem --enrollment.profile tls --csr.hosts "apmi5,localhost,$IP" --mspdir tls-ca/$1/msp

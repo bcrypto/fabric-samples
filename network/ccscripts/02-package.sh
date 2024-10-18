f41c9f6136d829076b5961d36de62b44235883a2
Mikhail Mitskevich <mitskevichmn@gmail.com>
2024-10-18 15:18:24 +0300

    Compile and package Java SC

---
+++ network/ccscripts/02-package.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+SCRIPTDIR="$(dirname "$(realpath "$0")")"
+source $SCRIPTDIR/utils.sh
+export PATH="$(dirname $(readlink -e ./))/bin:$PATH"
+export FABRIC_CFG_PATH=$PWD/../config/
+
+CC_NAME=$1
+CC_SRC_PATH=$2
+CC_RUNTIME_LANGUAGE=$3
+CC_VERSION=$4
+
+packageChaincode() {
+  set -x
+  peer lifecycle chaincode package ${CC_NAME}.tar.gz --path ${CC_SRC_PATH} --lang ${CC_RUNTIME_LANGUAGE} --label ${CC_NAME}_${CC_VERSION} >&log.txt
+  res=$?
+  PACKAGE_ID=$(peer lifecycle chaincode calculatepackageid ${CC_NAME}.tar.gz)
+  { set +x; } 2>/dev/null
+  cat log.txt
+  verifyResult $res "Chaincode packaging has failed"
+  successln "Chaincode is packaged"
+}
+
+function checkPrereqs() {
+  jq --version > /dev/null 2>&1
+
+  if [[ $? -ne 0 ]]; then
+    errorln "jq command not found..."
+    errorln
+    errorln "Follow the instructions in the Fabric docs to install the prereqs"
+    errorln "https://hyperledger-fabric.readthedocs.io/en/latest/prereqs.html"
+    exit 1
+  fi
+}
+
+#check for prerequisites
+checkPrereqs
+
+## package the chaincode
+packageChaincode

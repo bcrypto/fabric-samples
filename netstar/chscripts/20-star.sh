11619d72322ebd87109b0b1521fd5070e25be7f4
Mikhail Mitskevich <mitskevichmn@gmail.com>
2024-12-20 13:54:06 +0300

    Init all channels

---
+++ netstar/chscripts/20-star.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+SCRIPTDIR="$(dirname "$(realpath "$0")")"
+source $SCRIPTDIR/../utils.sh
+export PATH="$(dirname $(readlink -e ./))/bin:$PATH"
+
+for (( k = 1; k < $ORG_COUNT + 1; ++k )); do
+    for (( j = k + 1; j < $ORG_COUNT + 1; ++j )); do
+        source $SCRIPTDIR/10-channel.sh Org$k Org$j
+    done
+done
